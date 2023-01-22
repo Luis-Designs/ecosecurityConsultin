71953d750ce05ab631a1f91b584d643b83ebbadc
luis-designs <luislozano120xd@gmail.com>
2023-01-22 18:59:34 -0500

    feat: deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <p>Piero es un crack:D</p>
+        <p>Ya salió :D</p>
       </main>
     </>
   )
