c099038edbacd103b21e49538760b8f1e8344405
Luis-Designs <64873592+Luis-Designs@users.noreply.github.com>
2023-01-23 00:00:32 +0000

    Update gh-pages to output generated at 71953d7

--- _next/static/dcpQimk6PHam1o3311J-R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bff950a31bfd5d70.css","static/chunks/pages/index-12bb5198e895bc9c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
